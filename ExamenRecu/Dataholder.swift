11fcc4684c3f1f0b28a1088d9aee709cef63094a
LUCAS PAJARES PRIETO <lucas.pajares@utmac0013.local>
2018-06-19 11:49:36 +0200

    registroMasoMenos

--- ExamenRecu/Dataholder.swift
+++ ExamenRecu/Dataholder.swift
@@ -7,7 +7,132 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseStorage
 class Dataholder: NSObject {
-
+    static let sharedInstance:DataHolder = DataHolder()
+    
+    var fireStoreDB:Firestore?
+    
+    var miPerfil:Perfil = Perfil()
+    var fireStorage:Storage?
+    var HMIMG :[String: UIImage]?=[:]
+    var arCiudades:[City] = []
+    
+    var user:String = ""
+    var email:String = ""
+    var pass:String = ""
+    var repass:String = ""
+    
+    func initFirebase(){
+        FirebaseApp.configure()
+        fireStoreDB = Firestore.firestore()
+        fireStorage = Storage.storage()
+        let citiesRef = fireStoreDB?.collection("coordenadas")
+        
+ }
+    func descargarCiudades(delegate:DataHolderDelegate){
+        
+        DataHolder.sharedInstance.fireStoreDB?.collection("cities").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+                delegate.DHDDescargaCiudadesCompleta!(blnFin: false)
+            } else {
+                self.arCiudades=[]
+                for document in querySnapshot!.documents {
+                    let ciudad:City = City()
+                    ciudad.sID=document.documentID
+                    ciudad.setMap(valores: document.data())
+                    self.arCiudades.append(ciudad)
+                    print("\(document.documentID) => \(document.data())")
+                }
+                print("----->>>> ",self.arCiudades.count)
+                delegate.DHDDescargaCiudadesCompleta!(blnFin: true)
+            }
+            //self.tbMiTabla?.reloadData()
+        }
+        
+    }
+    func Login(delegate:DataHolderDelegate, sEmail:String, sContrasena:String) {
+        print("Hola " + sEmail)
+        
+        Auth.auth().signIn(withEmail: sEmail, password: sContrasena) {(email, error) in
+            if sEmail != ""{
+                
+                let ruta = DataHolder.sharedInstance.fireStoreDB?.collection("Perfiles").document((email?.uid)!)
+                
+                ruta?.getDocument { (document, error) in
+                    if document != nil{
+                        
+                        DataHolder.sharedInstance.miPerfil.setMap(valores: (document?.data())!)
+                        
+                        delegate.dataHolderLogin!(blfin: true)
+                        
+                    }
+                    else{
+                        print(error!)
+                    }
+                }
+            }
+            else{
+                print("Fallo al logearse")
+                delegate.dataHolderLogin!(blfin: false)
+            }
+        }
+    }
+    
+    func Registro(delegate:DataHolderDelegate,sEmail:String, sPass:String) {
+        Auth.auth().createUser(withEmail: email, password: pass){
+            (email, error)in
+            if self.pass != self.repass{
+                print("Las contraseñas no son iguales")
+            }
+            else if self.email != "" && self.user != ""{
+                print ("Te registraste")
+                
+                DataHolder.sharedInstance.fireStoreDB?.collection("Perfiles").document((email?.uid)!).setData(["email"
+                    :self.email, "nombre":self.user])
+                delegate.dataHolderRegister!(blfin: true)
+            }
+            else{
+                print(error!)
+                delegate.dataHolderRegister!(blfin: false)
+            }
+        }
+        
+    }
+    
+    func executeimagen(clave:String, delegate:DataHolderDelegate){
+        if self.HMIMG![clave] == nil{
+            let gsReference = self.fireStorage?.reference(forURL: clave)
+            gsReference?.getData(maxSize: 1 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print(error!)
+                }
+                else{
+                    let imgDescargada = UIImage(data: data!)
+                    self.HMIMG?[clave] = imgDescargada
+                    delegate.imagen!(imagen: imgDescargada!)
+                    
+                }
+            }
+            )
+            
+        }
+        else{
+            delegate.imagen!(imagen:self.HMIMG![clave]!)
+        }
+        print("llego")
+        
+    }
+    
+    
 }
+@objc protocol DataHolderDelegate{
+    @objc optional func DHDDescargaCiudadesCompleta(blnFin:Bool)
+    @objc optional func dataHolderRegister(blfin:Bool)
+    @objc optional func dataHolderLogin(blfin:Bool)
+    @objc optional func imagen(imagen:UIImage)
+}
+
+
--- ExamenRecu/RegisterViewController.swift
+++ ExamenRecu/RegisterViewController.swift
@@ -8,19 +8,43 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController,DataHolderDelegate {
+    @IBOutlet var txtEmail:UITextField?
+    @IBOutlet var txtUser:UITextField?
+    @IBOutlet var txtPassword:UITextField?
+    @IBOutlet var txtRePassword:UITextField?
+    @IBOutlet var btnAceptar:UIButton?
+    @IBOutlet var btnCancelar:UIButton?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        txtEmail?.text = DataHolder.sharedInstance.email
+        txtUser?.text = DataHolder.sharedInstance.user
+        txtPassword?.text = DataHolder.sharedInstance.pass
+        txtRePassword?.text = DataHolder.sharedInstance.repass
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
-    
+    @IBAction func clickRegister(){
+        
+        DataHolder.sharedInstance.email = (txtEmail?.text)!
+        DataHolder.sharedInstance.user = (txtUser?.text)!
+        DataHolder.sharedInstance.pass = (txtPassword?.text)!
+        DataHolder.sharedInstance.repass = (txtRePassword?.text)!
+        
+        
+        DataHolder.sharedInstance.Registro(delegate: self, sEmail: (txtEmail?.text)!, sPass: (txtPassword?.text)!)
+        
+        
+    }
+    func dataHolderRegister(blfin: Bool) {
+        if blfin==true{
+            self.performSegue(withIdentifier: "trregistro", sender: self)
+        }
 
     /*
     // MARK: - Navigation
@@ -32,4 +56,5 @@ class RegisterViewController: UIViewController {
     }
     */
 
+}
 }
