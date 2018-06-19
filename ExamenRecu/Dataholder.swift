c885912215cee296778c777031fd1fb414a9cd3e
LUCAS PAJARES PRIETO <lucas.pajares@utmac0013.local>
2018-06-19 11:36:54 +0200

    esqueleto

---
+++ ExamenRecu/Dataholder.swift
@@ -0,0 +1,13 @@
+//
+//  Dataholder.swift
+//  ExamenRecu
+//
+//  Created by LUCAS PAJARES PRIETO on 19/6/18.
+//  Copyright © 2018 LUCAS PAJARES PRIETO. All rights reserved.
+//
+
+import UIKit
+
+class Dataholder: NSObject {
+
+}
